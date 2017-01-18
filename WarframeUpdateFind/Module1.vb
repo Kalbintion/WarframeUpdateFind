2a8625e7793d85970b0e03c25b04fc0141c0ac44
Kalbintion <Distantimage24@gmail.com>
2017-01-17 20:28:11 -0600

    Only prints {{Ver}} tags for versions it found matching elements for

--- WarframeUpdateFind/Module1.vb
+++ WarframeUpdateFind/Module1.vb
@@ -12,12 +12,26 @@
         For i = 1 To cmdLineArgs.Length - 1
             toFind = toFind & " " & cmdLineArgs(i)
         Next
+        If toFind.Length > 1 Then toFind = toFind.Substring(1, toFind.Length - 1)
 
         Console.WriteLine("Looking for: " + toFind)
 
+        Dim verString As String = ""
+        Dim hasWrittenVerString As Boolean = False
+
         For i = 0 To patchLines.Length - 1
-            If patchLines(i).ToLower().Contains("update number") Then Console.WriteLine(System.Text.RegularExpressions.Regex.Replace(patchLines(i), "\|update number\s*?\=\s*?", "{{ver|", Text.RegularExpressions.RegexOptions.IgnoreCase) & "}}")
-            If patchLines(i).ToLower().Contains(toFind) Then Console.WriteLine(patchLines(i))
+            If patchLines(i).ToLower().Contains("update number") Then
+                verString = System.Text.RegularExpressions.Regex.Replace(patchLines(i), "\|update number\s*?\=\s*?", "{{ver|", Text.RegularExpressions.RegexOptions.IgnoreCase) & "}}"
+                hasWrittenVerString = False
+            End If
+            If patchLines(i).ToLower().Contains(toFind) Then
+                If hasWrittenVerString = False Then
+                    Console.WriteLine(verString)
+                    hasWrittenVerString = True
+                End If
+                Console.WriteLine(patchLines(i))
+            End If
+
         Next
     End Sub
 
